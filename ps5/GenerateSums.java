fcb60020964520bb1e8384495ab34a4b8efc47ef
Nathan Chang <nathanmacair2@Nathans-MacBook-Air.local>
2025-03-23 21:34:23 -0400

    added hw 5

---
+++ ps5/GenerateSums.java
@@ -0,0 +1,21 @@
+public class GenerateSums {
+    public static String generateSums(int n){
+        if (n == 0) return "";
+        if (n == 1) return "1";
+        String res = "";
+        int prefixSum = 0;
+        String prefixString = "1";
+        for (int i = 1; i <= n; i++){
+            prefixSum += i;
+            if (i != 1) prefixString = prefixString + " + " + i;
+            if (i == 1) res = res + prefixSum + '\n';
+            else if (i == n) res = res + prefixString + " = " + prefixSum;
+            else res = res + prefixString + " = " + prefixSum + '\n';
+        
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        System.out.println(generateSums(1));
+    }
+}
